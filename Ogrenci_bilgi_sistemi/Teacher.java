64e896e97efdcd7dca6628a49cdadd1017c43b52
Mehmet Ali SÜRÜCÜ <tekmas83@gmail.com>
2023-03-22 18:56:50 +0300

    ogrenci_bilgi_sistemi adında bir package eklendi

---
+++ Ogrenci_bilgi_sistemi/Teacher.java
@@ -0,0 +1,13 @@
+package patica.Ogrenci_bilgi_sistemi;
+
+public class Teacher {
+    String name;
+    String mpno;
+    String branch;
+
+    public Teacher(String name, String mpno, String branch) {
+        this.name = name;
+        this.mpno = mpno;
+        this.branch = branch;
+    }
+}
